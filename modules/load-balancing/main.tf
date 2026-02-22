cef6871cc80733cb07728435ff6f3ebfb81ef6c9
Bálint Molnár <baalint.molnar@gmail.com>
2026-02-22 19:40:47 +0100

    feat: installing NGINX Ingress Controller onto cluster via Terraform

---
+++ modules/load-balancing/main.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "nginx" {
+  chart = "ingress-nginx"
+  name  = "ingress-nginx"
+  namespace = "ingress-nginx"
+  create_namespace = true
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  depends_on = [var.cluster_name]
+
+  values = [
+    yamlencode({
+      controller = {
+        service = {
+          annotations = {
+            "service.beta.kubernetes.io/aws-load-balancer-type"   = "nlb"
+            "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
+          }
+        }
+      }
+    })
+  ]
+}
